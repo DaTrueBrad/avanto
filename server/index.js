c669ad3e1b1014782052f8e867c34f6a0527a52a
DaTrueBrad <bottbrady@gmail.com>
2023-01-10 15:58:44 -0700

    cars to front end

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const db = require('./util/database')
+const {Car, Photo, User, Inquiry} = require('./util/models')
+const seed = require('./util/seed')
+const {getAllCars} = require('./controllers/car')
+
+const server = express()
+server.use(express.json())
+server.use(cors())
+
+// ! Associations
+Car.hasMany(Photo)
+Photo.belongsTo(Car)
+
+User.hasMany(Inquiry)
+Inquiry.belongsTo(User)
+
+Car.hasMany(Inquiry)
+Inquiry.belongsTo(Car)
+
+//! endpoints
+server.get('/api/allCars', getAllCars)
+
+db
+  // .sync({force: true})
+  // .then(() => seed())
+server.listen(4000, () => console.log('Up on 4000'))
