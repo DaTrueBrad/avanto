219c9135ef0eb563c32e3d8e0ff29316b112ab51
DaTrueBrad <bottbrady@gmail.com>
2023-01-13 10:00:14 -0700

    login logout

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import HomeScreen from './screens/HomeScreen';
 import AuthScreen from './screens/AuthScreen';
 import ProductScreen from './screens/ProductScreen';
 import AdminScreen from './screens/AdminScreen';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 function App() {
+  const admin = useSelector(state => state.admin)
+  const userId = useSelector(state => state.userId)
+   
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/auth" element={<AuthScreen />} />
+        <Route path="/" element={!admin ? <HomeScreen /> : <Navigate to='/admin' />} />
+        <Route path="/auth" element={!userId ? <AuthScreen /> : <Navigate to='/' />} />
         <Route path="/product/:id" element={<ProductScreen />} />
-        <Route path="/admin" element={<AdminScreen />} />
+        <Route path="/admin" element={admin ? <AdminScreen /> : <Navigate to="/" />} />
       </Routes>
     </div>
   );
